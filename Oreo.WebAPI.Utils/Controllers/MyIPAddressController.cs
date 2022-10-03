30de16fb5708131b785580a78e1db77e8806efb9
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2022-10-03 14:37:32 -0700

    Added MyIPAddressController

---
+++ Oreo.WebAPI.Utils/Controllers/MyIPAddressController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oreo.WebAPI.Utils.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyIPAddressController : ControllerBase
+    {
+        // GET: api/MyIPAddress
+        [HttpGet]
+        public string Get()
+        {
+            return HttpContext.Connection.RemoteIpAddress.ToString();
+        }
+    }
+}
--- Oreo.WebAPI.Utils/Controllers/WeatherForecastController.cs
+++ Oreo.WebAPI.Utils/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Oreo.WebAPI.Utils.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
