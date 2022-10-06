c7c3cba211bd68d5d8fd23c9d3304eb20faa2b40
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2022-10-06 13:29:23 -0700

    Initial commit

---
+++ Oreo.WebAPI.OpenWeather/Controllers/GeoLocationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using RestSharp;
+using Newtonsoft.Json;
+
+using Oreo.WebAPI.OpenWeather.Models;
+
+namespace Oreo.WebAPI.OpenWeather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoLocationController : ControllerBase
+    {
+        private readonly ILogger<GeoLocationController> _log;
+
+        public GeoLocationController(ILogger<GeoLocationController> logger)
+        {
+            _log = logger;
+        }
+
+        /// <summary>
+        /// GET: api/GeoLocation
+        /// </summary>
+        /// <param name="Location"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<GeoLocation> Get(string Location)
+        {
+
+            RestClient client = new RestClient("http://api.openweathermap.org/geo/1.0");
+            RestRequest request = new RestRequest("direct?q=piedmont,ca,us&limit=1&appid=e850980c6457ad9e1f2fc72792dd0eca");
+            RestResponse response = await client.GetAsync(request);
+
+            GeoLocation locationResonse = JsonConvert.DeserializeObject<GeoLocation>(response.Content);
+
+            return locationResonse;
+        }        
+    }
+}
---
+++ Oreo.WebAPI.OpenWeather/Models/GeoLocation.cs
@@ -0,0 +1,11 @@
+﻿namespace Oreo.WebAPI.OpenWeather.Models
+{
+    public class GeoLocation
+    {
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Lat { get; set; }
+        public string Log { get; set; }
+    }
+}
